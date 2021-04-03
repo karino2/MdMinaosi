2e43680bcd35ba3aa45628fd9769d3df932e65c3
Kazuma Arino <hogeika2@gmail.com>
2021-04-03 12:36:25 +0900

    First edit functinality done.

--- main.js
+++ main.js
@@ -4,6 +4,56 @@ const { Remarkable } = require('remarkable')
 const remark = new Remarkable()
 const fs = require('fs/promises')
 
+const g_lineMap = new Map()
+let g_srcLines = [];
+let g_currentPath = '';
+
+// store line number
+remark.renderer.rules.paragraph_open = (tokens, idx) => {
+    let line;
+    if (tokens[idx].lines && tokens[idx].level == 0)
+    {
+        line = tokens[idx].lines[0]
+        g_lineMap[line] = tokens[idx].lines
+        return `<p src-line="${line}">`
+    }
+    return '<p>'
+}
+
+remark.renderer.rules.heading_open = (tokens, idx) => {
+    let line;
+    if (tokens[idx].lines && tokens[idx].level == 0)
+    {
+        line = tokens[idx].lines[0]
+        g_lineMap[line] = tokens[idx].lines
+        return `<h${tokens[idx].hLevel} src-line="${line}">`
+    }
+    return `<h${tokens[idx].hLevel}>`
+}
+
+remark.renderer.rules.bullet_list_open = (tokens, idx) => {
+    let line;
+    if (tokens[idx].lines && tokens[idx].level == 0)
+    {
+        line = tokens[idx].lines[0]
+        g_lineMap[line] = tokens[idx].lines
+        return `<ul src-line="${line}">`
+    }
+    return '<ul>'
+}
+
+remark.renderer.rules.ordered_list_open = (tokens, idx) => {
+    let line;
+    if (tokens[idx].lines && tokens[idx].level == 0)
+    {
+        line = tokens[idx].lines[0]
+        g_lineMap[line] = tokens[idx].lines
+        return `<ol src-line="${line}">`
+    }
+    return '<ol>'
+}
+
+
 function createWindow () {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize
     const win = new BrowserWindow({
@@ -33,6 +83,25 @@ app.on('window-all-closed', () => {
     }
 })
 
+const updateMd = (src, sender) => {
+    g_lineMap.clear()
+    sender.send('update-md', remark.render(src))
+}
+
+const openPath = async (filePath, sender) => {
+    g_currentPath = filePath
+    const cont = await fs.readFile( filePath )
+    const src = cont.toString()
+    g_srcLines = src.split('\n')
+    updateMd(src, sender)
+}
+
+ipcMain.on('setup-done', async (event)=> {
+    /*
+    openPath( "/Users/arinokazuma/work/GitHub/karino2.github.io/_posts/2021-04-02-MDMinaosi.md",
+     event.sender )
+     */
+})
 
 ipcMain.on('open-file-dialog', async (event)=>{
     const {canceled, filePaths} = await dialog.showOpenDialog({
@@ -40,7 +109,31 @@ ipcMain.on('open-file-dialog', async (event)=>{
         filters: [{ name: 'Markdown', extensions: ['md'] }]
     })
     if(!canceled) {
-        const cont = await fs.readFile( filePaths[0] )
-        event.sender.send('update-md', remark.render(cont.toString()))
+        console.log(filePaths[0])
+        openPath( filePaths[0], event.sender )
     }
+})
+
+ipcMain.on('line-click', async (event, line)=>{
+    let target = []
+    let [start, end] = g_lineMap[line]
+    for( let i = start; i < end; i++ ) {
+        target.push( g_srcLines[i] )
+    }
+    event.sender.send('start-edit', target.join('\n'), [start, end])
+})
+
+ipcMain.on('submit', async (event, text, [start, end])=>{
+    let prev = g_srcLines
+    g_srcLines = []
+    for (let i = 0; i < start; i++) {
+        g_srcLines[i] = prev[i]
+    }
+    text.split('\n').forEach(line => g_srcLines.push(line))
+    for(let i = end; i < prev.length; i++) {
+        g_srcLines.push(prev[i])
+    }
+    let src = g_srcLines.join('\n')
+    await fs.writeFile(g_currentPath, src)
+    updateMd(src, event.sender)
 })
--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const {ipcRenderer} = require('electron')
 
+
 window.addEventListener('DOMContentLoaded', () => {
 
   document.getElementById('open-file').addEventListener('click', (event)=>{
@@ -9,7 +10,65 @@ window.addEventListener('DOMContentLoaded', () => {
   const mdDiv = document.getElementById('md-div')
 
   ipcRenderer.on('update-md', (event, html) => {
-    mdDiv.innerHTML = html;
+    mdDiv.innerHTML = html
   })
+
+  let lastSelected = null
+  let targetRange = [0, 0]
+
+  const onBodyClick = (event) => {
+    const findTaretTopElem = (start) => {
+      if (start.tagName == "body")
+        return null
+      if (start == mdDiv)
+        return null
+      let cur = start
+      let prev = cur
+      while (cur != mdDiv) {
+        let sline = cur.getAttribute('src-line')
+        if (sline != null)
+          return cur
+        prev = cur
+        cur = cur.parentElement
   
+        // not md-div child
+        if (cur == null)
+          return null
+      }
+      return prev
+    }
+    let topelem = findTaretTopElem(event.target) 
+    if (!topelem)
+      return
+    const sline = topelem.getAttribute('src-line')
+    if (sline != null)
+    {
+      lastSelected = topelem
+      ipcRenderer.send("line-click", parseInt(sline))
+    }
+  }
+
+  document.getElementById("body").addEventListener('click', onBodyClick)
+
+  const editDiv = document.getElementById("edit-div")
+  const editArea = editDiv.querySelector(".edit-area")
+
+  ipcRenderer.on('start-edit', (event, html, [start, end]) => {
+    targetRange = [start, end]
+    lastSelected.insertAdjacentElement('afterend', editDiv)
+    editArea.value = html
+    editArea.rows = Math.max((end-start), 3);
+    editDiv.style.display = 'block'
+  })
+  document.getElementById('cancel-edit').addEventListener('click', ()=>{
+    editDiv.style.display = 'none'
+  })
+
+  document.getElementById('submit-edit').addEventListener('click', ()=>{
+    ipcRenderer.send('submit', editArea.value, targetRange)
+    editDiv.style.display = 'none'
+  })
+
+  ipcRenderer.send("setup-done")
+
 })
